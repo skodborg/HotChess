72f60e065117e6c36f4c3e5589845082e72142ab
skodborg <skod92@gmail.com>
2013-12-07 20:42:14 +0100

    Implemented turns, added testlist, specified alphaChess requirements, began winner determining

--- src/GameImpl.java
+++ src/GameImpl.java
@@ -2,14 +2,20 @@
  * Created by marc on 07/12/13.
  */
 public class GameImpl implements Game {
+    private Color _playerInTurn;
+
+    public GameImpl() {
+        _playerInTurn = Color.WHITE;
+    }
+
     @Override
     public Color getWinner() {
-        return null;
+        return Color.NONE;
     }
 
     @Override
     public Color getPlayerInTurn() {
-        return null;
+        return _playerInTurn;
     }
 
     @Override
@@ -19,6 +25,11 @@ public class GameImpl implements Game {
 
     @Override
     public boolean movePiece(Position from, Position to) {
+        if (_playerInTurn == Color.BLACK) {
+            _playerInTurn = Color.WHITE;
+        } else {
+            _playerInTurn = Color.BLACK;
+        }
         return false;
     }
 }
--- test/TestAlphaChess.java
+++ test/TestAlphaChess.java
@@ -1,5 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
+import sun.jvm.hotspot.code.Location;
+
+import java.util.WeakHashMap;
+
 import static org.junit.Assert.*;
 
 /**
@@ -19,5 +23,29 @@ public class TestAlphaChess {
         assertEquals("White player is in turn first in every game of chess",
                 Color.WHITE, _game.getPlayerInTurn());
 
+    }
+
+    @Test
+    public void shouldLetBlackPlayerBeInTurnWhenWhiteHasMoved() {
+        _game.movePiece(Position.A2, Position.A3);
+        assertEquals("Black player should be in turn when white has moved",
+                Color.BLACK, _game.getPlayerInTurn());
+    }
+
+    @Test
+    public void shouldLetWhitePlayerBeInTurnWhenBlackHasMoved() {
+        // White player moves his pawn from A2 to A3
+        _game.movePiece(Position.A2, Position.A3);
+        // Black player moves his pawn from A7 to A6
+        _game.movePiece(Position.A7, Position.A6);
+
+        assertEquals("White player should be in turn when black has moved",
+                Color.WHITE, _game.getPlayerInTurn());
+    }
+
+    @Test
+    public void shouldNotFindAWinnerInRoundOne() {
+        assertEquals("No winner should have been found in round one",
+                Color.NONE, _game.getWinner());
     }
 }

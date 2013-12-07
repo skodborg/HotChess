a3176669833233f670d668d614459102c379c9da
skodborg <skod92@gmail.com>
2013-12-07 22:40:19 +0100

    Started TestDeltaChess

--- test/TestDeltaChess.java
+++ test/TestDeltaChess.java
@@ -1,5 +1,14 @@
+import org.junit.Before;
+
 /**
  * Created by marc on 07/12/13.
  */
 public class TestDeltaChess {
+
+    private Game _game;
+
+    @Before
+    public void setup() {
+        _game = new GameImpl(new SimpleBoardSetupStrategy());
+    }
 }

0ffeba8d43f1df27a45959cda9711024c01fbd2c
skodborg <skod92@gmail.com>
2014-02-25 17:31:46 +0100

    ADD: Pawn now moves and attacks correctly

--- src/Production/BlackPawnMoveRuleStrategy.java
+++ src/Production/BlackPawnMoveRuleStrategy.java
@@ -7,7 +7,28 @@ public class BlackPawnMoveRuleStrategy implements PieceMoveRuleStrategy {
     @Override
     public Iterator<BoardPosition> iterator(BoardPosition from, Game game) {
         ArrayList<BoardPosition> validPositions = new ArrayList<BoardPosition>();
-        validPositions.add(BoardPosition.south(from));
+        Color movingPieceColor = game.getPieceAtPosition(from).getColor();
+
+        // check that no piece occupies the next vertical position
+        BoardPosition nextPos = BoardPosition.south(from);
+        if (game.getPieceAtPosition(nextPos) == null) {
+            validPositions.add(BoardPosition.south(from));
+        }
+
+        // add diagonal positions if an attack is possible
+        nextPos = BoardPosition.southEast(from);
+        Piece targetPiece = game.getPieceAtPosition(nextPos);
+        if (targetPiece != null &&
+                targetPiece.getColor() != movingPieceColor) {
+            validPositions.add(nextPos);
+        }
+        nextPos = BoardPosition.southWest(from);
+        targetPiece = game.getPieceAtPosition(nextPos);
+        if (targetPiece != null &&
+                targetPiece.getColor() != movingPieceColor) {
+            validPositions.add(nextPos);
+        }
+
         return validPositions.iterator();
     }
 }
--- src/Production/WhitePawnMoveRuleStrategy.java
+++ src/Production/WhitePawnMoveRuleStrategy.java
@@ -7,7 +7,28 @@ public class WhitePawnMoveRuleStrategy implements PieceMoveRuleStrategy {
     @Override
     public Iterator<BoardPosition> iterator(BoardPosition from, Game game) {
         ArrayList<BoardPosition> validPositions = new ArrayList<BoardPosition>();
-        validPositions.add(BoardPosition.north(from));
+        Color movingPieceColor = game.getPieceAtPosition(from).getColor();
+
+        // check that no piece occupies the next vertical position
+        BoardPosition nextPos = BoardPosition.north(from);
+        if (game.getPieceAtPosition(nextPos) == null) {
+            validPositions.add(BoardPosition.north(from));
+        }
+
+        // add diagonal positions if an attack is possible
+        nextPos = BoardPosition.northEast(from);
+        Piece targetPiece = game.getPieceAtPosition(nextPos);
+        if (targetPiece != null &&
+                targetPiece.getColor() != movingPieceColor) {
+            validPositions.add(nextPos);
+        }
+        nextPos = BoardPosition.northWest(from);
+        targetPiece = game.getPieceAtPosition(nextPos);
+        if (targetPiece != null &&
+                targetPiece.getColor() != movingPieceColor) {
+            validPositions.add(nextPos);
+        }
+
         return validPositions.iterator();
     }
 }

3aa60785917fc603637abd9c1f83e986896b03f9
skodborg <skod92@gmail.com>
2014-02-25 17:19:20 +0100

    ADD: King now attacks properly

--- src/Production/KingMoveRuleStrategy.java
+++ src/Production/KingMoveRuleStrategy.java
@@ -8,32 +8,47 @@ public class KingMoveRuleStrategy implements PieceMoveRuleStrategy {
     @Override
     public Iterator<BoardPosition> iterator(BoardPosition from, Game game) {
         Set<BoardPosition> validPositions = new TreeSet<BoardPosition>();
+        Piece movingPiece = game.getPieceAtPosition(from);
         BoardPosition currentPos;
-        if ((currentPos = BoardPosition.north(from)) != null) {
+        if ((currentPos = BoardPosition.north(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.northEast(from)) != null) {
+        if ((currentPos = BoardPosition.northEast(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.east(from)) != null) {
+        if ((currentPos = BoardPosition.east(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.southEast(from)) != null) {
+        if ((currentPos = BoardPosition.southEast(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.south(from)) != null) {
+        if ((currentPos = BoardPosition.south(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.southWest(from)) != null) {
+        if ((currentPos = BoardPosition.southWest(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.west(from)) != null) {
+        if ((currentPos = BoardPosition.west(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
-        if ((currentPos = BoardPosition.northWest(from)) != null) {
+        if ((currentPos = BoardPosition.northWest(from)) != null
+                && onlyValidAttacks(game, movingPiece, currentPos)) {
             validPositions.add(currentPos);
         }
 
         return validPositions.iterator();
+    }
+
+    // returns true if the target position is not occupied, or contains a piece of the opposing players color
+    private boolean onlyValidAttacks(Game game, Piece movingPiece, BoardPosition targetPosition) {
+        Piece targetPiece = game.getPieceAtPosition(targetPosition);
+        return targetPiece == null || targetPiece.getColor() != movingPiece.getColor();
     }
 }
